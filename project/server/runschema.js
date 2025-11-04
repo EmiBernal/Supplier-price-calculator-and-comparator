f38530fca7a44a0d17a6004848b00d2214b77b98
Emiliano Bernal <emiibernal1275@gmail.com>
2025-11-04 10:51:43 -0300

    Script para crear una nueva tabla

---
+++ project/server/runschema.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+async function runSchema() {
+  try {
+    console.log("🧩 Ejecutando schema.sql...");
+    const sql = fs.readFileSync("./schema.sql", "utf8");
+    await pool.query(sql);
+    console.log("✅ Esquema creado correctamente en la base de datos");
+  } catch (err) {
+    console.error("❌ Error al ejecutar schema.sql:", err);
+  } finally {
+    await pool.end();
+  }
+}
+
+runSchema();
